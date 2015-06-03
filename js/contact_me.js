12c8c3e0b0c93f753b4c9026f12e7e6d61a0dc65
webplug <webplug@gmail.com>
2015-06-03 18:36:20 +0900

    1
    1

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,15 +1,5 @@
 $(function() {
 
-   $("input,textarea").jqBootstrapValidation({
-        preventSubmit: true,
-        submitError: function($form, event, errors) {
-            // additional error messages or events
-        },
-        filter: function() {
-            return $(this).is(":visible");
-        },
-    });
-
     $("a[data-toggle=\"tab\"]").click(function(e) {
         e.preventDefault();
         $(this).tab("show");
